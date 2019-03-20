d0d657bc4b85c7a16e30c34a16ea0dc2f2ee3ad5
lucas7788 <sishsh@163.com>
2019-03-20 15:07:22 +0800

    add basic type impl decode2

--- examples/token-zero-copy/src/lib.rs
+++ examples/token-zero-copy/src/lib.rs
@@ -1,10 +1,10 @@
 #![no_std]
 extern crate ontio_std as ostd;
 
-use ostd::abi::{Encoder, Sink, Source, ZeroCopySource};
-use ostd::types::{Addr, Address, U256};
+use ostd::abi::{Encoder, Sink, ZeroCopySource};
+use ostd::types::{Addr, U256};
 use ostd::{database, runtime};
-use ostd::{string::ToString, String};
+use ostd::{string::ToString};
 
 const KEY_TOTAL_SUPPLY: &'static str = "total_supply";
 const NAME: &'static str = "wasm_token";
@@ -24,15 +24,15 @@ fn transfer(from: &Addr, to: &Addr, amount: U256) -> bool {
     if runtime::check_witness(&from) == false {
         return false;
     }
-    let mut frmbal = balance_of(from);
-    let mut tobal = balance_of(to);
+    let mut frmbal = balance_of(&from);
+    let mut tobal = balance_of(&to);
     if amount == 0.into() || frmbal < amount {
         false
     } else {
         frmbal = frmbal - amount;
         tobal = tobal + amount;
-        database::put(from, frmbal);
-        database::put(to, tobal);
+        database::put(&from, frmbal);
+        database::put(&to, tobal);
         notify(("Transfer".to_string(), from, to, amount));
         true
     }
@@ -46,7 +46,7 @@ fn total_supply() -> U256 {
 pub fn invoke() {
     let input = runtime::input();
     let mut source = ZeroCopySource::new(&input);
-    let action: &[u8] = source.read().unwrap();
+    let action:&[u8] = source.read().unwrap();
     let mut sink = Sink::new(12);
     match action {
         b"init" => sink.write(initialize()),
